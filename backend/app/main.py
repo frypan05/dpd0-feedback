f99e2ae6aa2b9d6133d5093575aa0040f6e1678f
frypan05 <daksharma5804@gmail.com>
2025-06-30 09:45:22 +0530

    CORS updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://dpd0-feedback.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
